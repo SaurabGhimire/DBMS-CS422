a773f1d3b01b3e1e3389dd53480e0cbf6acc700f
Saurab Ghimire <sourrab@gmail.com>
2024-05-04 21:19:30 -0500

    insert into customers completed

---
+++ Lab5/problem2.sql
@@ -0,0 +1,10 @@
+-- Insert Data into Customers
+insert into Customers (name, contact_name, country) values ("Sagar Dahal", "Chaosifier", "USA");
+insert into Customers (name, contact_name, country) values ("Yogen Pokhrel", "Sarkar", "Dubai");
+insert into Customers (name, contact_name, country) values ("Bisho Silwal", "BishoNath Sahuji", "Germany");
+insert into Customers (name, contact_name, country) values ("Gaurav Neupane", "Guru", "Australia");
+insert into Customers (name, contact_name, country) values ("Roshan Bhattarai", "Ranchhoddas", "England");
+
+insert into Orders (customer_id, order_date, ship_city, ship_country) values
+(7, '2020-11-11', 'Fairfield', 'USA');
+
